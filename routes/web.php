42d3b72c9fb1bc68c9112c0265319ebb7b76550e
Dh3v1n <dhevintandiono123@gmail.com>
2025-12-11 09:10:36 +0700

    route restored

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,18 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('home', function () {
+    return view('home');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('events', function () {
+    return view('events');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
